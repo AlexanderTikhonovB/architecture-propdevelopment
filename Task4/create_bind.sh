55e5d47eaee1745d0f8bd85bddffe6e75c642219
tikhonovab <tikhonovab@mts.ru>
2026-02-27 10:40:25 +0300

    tmp

--- Task4/create_bind.sh
+++ Task4/create_bind.sh
@@ -1,5 +1,8 @@
-kubectl create rolebinding add_support  --clusterrole=support   --user=support 
+kubectl create clusterrolebindings add_support --clusterrole=support --user=support
 kubectl get rolebindings,clusterrolebindings -A -o wide | grep "support"
 
-kubectl create rolebinding add_develop  --clusterrole=develop   --user=dev  
+kubectl create rolebinding add_develop --clusterrole=develop --user=dev
 kubectl get rolebindings,clusterrolebindings -A -o wide | grep "develop" 
+
+kubectl create clusterrolebindings add_security --clusterrole=security --user=security
+kubectl get rolebindings,clusterrolebindings -A -o wide | grep "security"
--- Task4/create_roles.sh
+++ Task4/create_roles.sh
@@ -2,5 +2,5 @@ kubectl create role support --verb=get,list,watch --resource=pods
 
 kubectl create role develop --verb=create,update,patch,delete --resource=pods
 
-kubectl create role security --verb=get,list,create,update,delete,watch --resource=secrets, roles
+kubectl create role security --verb=get,list,create,update,delete,watch --resource=secrets,roles
 
